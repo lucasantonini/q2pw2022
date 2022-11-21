ee8cb5d93d134fef95a9cdc2c70fd3a651753264
lucasantonini <110941330+lucasantonini@users.noreply.github.com>
2022-11-21 16:54:53 -0300

    push final

--- proyecto-final/script.js
+++ proyecto-final/script.js
@@ -7,3 +7,206 @@ if (toastTrigger) {
     toast.show()
     })
 }
+
+
+// CARRITO
+const baseDeDatos = [
+{
+    id: 1,
+    nombre: 'Bundle Culinarias',
+    precio: 20000,
+    imagen: ''
+},
+{
+    id: 2,
+    nombre: 'Bundle Aventuras',
+    precio: 20000,
+    imagen: ''
+},
+{
+    id: 3,
+    nombre: 'Bundle Nocturnas',
+    precio: 10000,
+    imagen: ''
+},
+{
+    id: 4,
+    nombre: 'Bundle Hoteles',
+    precio: 15000,
+    imagen: ''
+},
+{
+    id: 5,
+    nombre: 'Cupon Regalo 1',
+    precio: 5000,
+    imagen: ''
+},
+{
+    id: 6,
+    nombre: 'Cupon Regalo 2',
+    precio: 2500,
+    imagen: ''
+},
+
+
+];
+
+let carrito = [];
+const divisa = '$';
+const DOMitems = document.querySelector('#items');
+const DOMcarrito = document.querySelector('#carrito');
+const DOMtotal = document.querySelector('#total');
+const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+const DOMbotonFinalizar = document.querySelector('#liveToastBtn2');
+
+function renderizarProductos() {
+    baseDeDatos.forEach((info) => {
+
+        const miNodo = document.createElement('div');
+        miNodo.classList.add('card', 'col-sm-5','g-3');
+
+        const miNodoCardBody = document.createElement('div');
+        miNodoCardBody.classList.add('card-body');
+
+        const miNodoTitle = document.createElement('h4');
+        miNodoTitle.classList.add('card-title');
+        miNodoTitle.textContent = info.nombre;
+
+        const miNodoImagen = document.createElement('img');
+        miNodoImagen.classList.add('img-fluid');
+        miNodoImagen.setAttribute('src', info.imagen);
+
+        const miNodoPrecio = document.createElement('h5');
+        miNodoPrecio.classList.add('card-text');
+        miNodoPrecio.textContent = `${info.precio}${divisa}`;
+
+        const miNodoBoton = document.createElement('button');
+        miNodoBoton.classList.add('btn', 'btn-dark');
+        miNodoBoton.textContent = '+';
+        miNodoBoton.setAttribute('marcador', info.id);
+        miNodoBoton.addEventListener('click', añadirProductoAlCarrito);
+
+        miNodoCardBody.appendChild(miNodoImagen);
+        miNodoCardBody.appendChild(miNodoTitle);
+        miNodoCardBody.appendChild(miNodoPrecio);
+        miNodoCardBody.appendChild(miNodoBoton);
+        miNodo.appendChild(miNodoCardBody);
+        DOMitems.appendChild(miNodo);
+    });
+}
+
+
+function añadirProductoAlCarrito(evento) {
+
+    carrito.push(evento.target.getAttribute('marcador'))
+
+    renderizarCarrito();
+
+}
+
+
+function renderizarCarrito() {
+
+    DOMcarrito.textContent = '';
+
+    const carritoSinDuplicados = [...new Set(carrito)];
+
+    carritoSinDuplicados.forEach((item) => {
+
+        const miItem = baseDeDatos.filter((itemBaseDatos) => {
+
+            return itemBaseDatos.id === parseInt(item);
+        });
+
+        const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+
+            return itemId === item ? total += 1 : total;
+        }, 0);
+
+        const miNodo = document.createElement('li');
+        miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+        miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
+
+        const miBoton = document.createElement('button');
+        miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+        miBoton.textContent = 'X';
+        miBoton.style.marginLeft = '1rem';
+        miBoton.dataset.item = item;
+        miBoton.addEventListener('click', borrarItemCarrito);
+
+        miNodo.appendChild(miBoton);
+        DOMcarrito.appendChild(miNodo);
+    });
+
+    DOMtotal.textContent = calcularTotal();
+}
+
+
+function borrarItemCarrito(evento) {
+
+    const id = evento.target.dataset.item;
+
+    carrito = carrito.filter((carritoId) => {
+        return carritoId !== id;
+    });
+
+    renderizarCarrito();
+}
+
+function calcularTotal() {
+
+    return carrito.reduce((total, item) => {
+
+        const miItem = baseDeDatos.filter((itemBaseDatos) => {
+            return itemBaseDatos.id === parseInt(item);
+        });
+
+        return total + miItem[0].precio;
+    }, 0).toFixed(2);
+}
+
+
+function vaciarCarrito() {
+
+    carrito = [];
+
+    renderizarCarrito();
+}
+
+function finalizarCompra() {
+
+    carrito = [];
+    total= 0;
+    renderizarCarrito();
+}
+
+DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+DOMbotonFinalizar.addEventListener('click', finalizarCompra);
+
+
+renderizarProductos();
+renderizarCarrito();
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alert = (message, type) => {
+const wrapper = document.createElement('div')
+wrapper.innerHTML = [
+`<div class="alert alert-${type} alert-dismissible" role="alert">`,
+`   <div>${message}</div>`,
+'   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+'</div>'
+].join('')
+
+alertPlaceholder.append(wrapper)
+}
+
+const toastTrigger2 = document.getElementById('liveToastBtn2')
+const toastLiveExample2 = document.getElementById('liveToast2')
+if (toastTrigger2) {
+toastTrigger2.addEventListener('click', () => {
+const toast = new bootstrap.Toast(toastLiveExample)
+
+toast.show()
+})
+}
